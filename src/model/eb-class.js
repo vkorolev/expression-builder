95926b389b1f99e7a8bf3f6e57ca28fe38b8e158
Valentin Korolev <kor.val@inbox.ru>
2016-05-23 11:51:27 +0300

    Watch on evaluated object instead of node attrs.

--- src/model/eb-class.js
+++ src/model/eb-class.js
@@ -11,44 +11,54 @@ module.exports = function (angular) {
             link: function (scope, element, attr) {
                 var getter = $parse(attr.ebClass),
                     classes = [];
-                
-                var unbind = scope.$watch('node.attributes', function () {
-                    var val = getter(scope),
-                        classesToRemove = classes.join(' ');
+
+                var unbind = scope.$watch(evaluateClassObject, function (value) {
+                    var classesToRemove = classes.join(' ');
+
                     classes = [];
-                    
-                    setClasses(val);
-                    
+
+                    setClasses(value);
+
                     element.removeClass(classesToRemove);
                     element.addClass(classes.join(' '));
                 }, true);
-                
+
                 scope.$on('$destroy', function () {
                     unbind();
                 });
-                
-                function setClasses (object) {
-                    if(!object) {
-                        return;
+
+                function evaluateClassObject() {
+                    var classObject = getter(scope),
+                        result = {};
+
+                    if (!classObject) {
+                        return result;
                     }
-                    
+
+                    var keys = Object.keys(classObject),
+                        length = keys.length;
+
+                    for (var i = 0; i < length; i++) {
+                        var key = keys[i],
+                            value = classObject[key];
+                        if (utils.isFunction(value)) {
+                            result[key] = value(scope.node);
+                        } else {
+                            result[key] = value;
+                        }
+                    }
+
+                    return result;
+                }
+
+                function setClasses(object) {
                     var keys = Object.keys(object),
                         length = keys.length;
-                    
-                    for(var i = 0; i < length; i++) {
+
+                    for (var i = 0; i < length; i++) {
                         var key = keys[i];
-                        setClass(key, object[key]);
-                    }
-                }
-                
-                function setClass(value, predicate) {
-                    if (utils.isFunction(predicate)) {
-                        if (predicate(scope.node)) {
-                            classes.push(value);
-                        }
-                    } else {
-                        if (predicate) {
-                            classes.push(value);
+                        if (object[key]) {
+                            classes.push(key);
                         }
                     }
                 }
